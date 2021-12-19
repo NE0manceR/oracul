f538865f72a4dce6471d24b28d5aea21064591e7
Alex <alex.krynytskyi@gmail.com>
2021-12-19 23:32:07 +0200

    Updates

---
+++ server/chunks/382.js
@@ -0,0 +1,393 @@
+exports.id = 382;
+exports.ids = [382];
+exports.modules = {
+
+/***/ 7278:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"footer": "footer_footer__LV2HF",
+	"footer_content": "footer_footer_content__aKWqm",
+	"move": "footer_move__WCXVX"
+};
+
+
+/***/ }),
+
+/***/ 2936:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"header": "header_header__GSKJx",
+	"navigation": "header_navigation__YgNBr",
+	"drop_down": "header_drop_down__ol808",
+	"bubble-anim": "header_bubble-anim__YbFgK",
+	"showMenu": "header_showMenu__pB2JG"
+};
+
+
+/***/ }),
+
+/***/ 6541:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "layout_container__B_Rgx",
+	"page_wrap": "layout_page_wrap__5P3i1"
+};
+
+
+/***/ }),
+
+/***/ 2449:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"logo_wrap": "logo_logo_wrap__lb9Tt",
+	"logo": "logo_logo__AXqd6",
+	"bubble-anim": "logo_bubble-anim__ct8E6",
+	"logo_text": "logo_logo_text__fJp_W",
+	"rotate-planet": "logo_rotate-planet__c_A5r"
+};
+
+
+/***/ }),
+
+/***/ 3772:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1664);
+/* harmony import */ var _logo__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2915);
+/* harmony import */ var _header_module_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(2936);
+/* harmony import */ var _header_module_scss__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_header_module_scss__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+const Header = ()=>{
+    return(/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+        className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().header),
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                href: "/user",
+                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_logo__WEBPACK_IMPORTED_MODULE_2__/* .Logo */ .T, {
+                    })
+                })
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("nav", {
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                    className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().navigation),
+                    children: [
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                            children: [
+                                "Top- 7",
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().drop_down),
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/0",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "BITCOIN/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/1",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Ethereum/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/2",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Ripple/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/3",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Shibain/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/4",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Tesla/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/5",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Alibaba/USD"
+                                            })
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+                                            href: "/ditail/6",
+                                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                                children: "Gazprom/USD"
+                                            })
+                                        })
+                                    ]
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                            children: [
+                                "Knowlage",
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().drop_down),
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Knowlage"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Knowlage"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Knowlage"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Knowlage"
+                                        })
+                                    ]
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                            children: [
+                                "Wider view",
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().drop_down),
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Wider view"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Wider view"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Wider view"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "Wider view"
+                                        })
+                                    ]
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                            children: [
+                                "What else",
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: (_header_module_scss__WEBPACK_IMPORTED_MODULE_3___default().drop_down),
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "What else"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "What else"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "What else"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                                            children: "What else"
+                                        })
+                                    ]
+                                })
+                            ]
+                        })
+                    ]
+                })
+            })
+        ]
+    }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Header);
+
+
+/***/ }),
+
+/***/ 6382:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ layout_Layout)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./components/header/index.js
+var header = __webpack_require__(3772);
+// EXTERNAL MODULE: ./components/footer/footer.module.scss
+var footer_module = __webpack_require__(7278);
+var footer_module_default = /*#__PURE__*/__webpack_require__.n(footer_module);
+;// CONCATENATED MODULE: ./components/footer/index.js
+
+
+const Footer = ()=>{
+    return(/*#__PURE__*/ jsx_runtime_.jsx("div", {
+        className: (footer_module_default()).footer,
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+            className: (footer_module_default()).footer_content,
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'RUB USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'EURO USD >'
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                    children: 'CNY USD >'
+                })
+            ]
+        })
+    }));
+};
+
+// EXTERNAL MODULE: ./components/layout/layout.module.scss
+var layout_module = __webpack_require__(6541);
+var layout_module_default = /*#__PURE__*/__webpack_require__.n(layout_module);
+;// CONCATENATED MODULE: ./components/layout/Layout.js
+
+
+
+
+const Layout = ({ children  })=>{
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: (layout_module_default()).container,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(header/* default */.Z, {
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (layout_module_default()).page_wrap,
+                children: children
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Footer, {
+            })
+        ]
+    }));
+};
+/* harmony default export */ const layout_Layout = (Layout);
+
+
+/***/ }),
+
+/***/ 2915:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "T": () => (/* binding */ Logo)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _logo_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2449);
+/* harmony import */ var _logo_module_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_logo_module_scss__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const Logo = ()=>{
+    return(/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: (_logo_module_scss__WEBPACK_IMPORTED_MODULE_1___default().logo_wrap),
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                className: (_logo_module_scss__WEBPACK_IMPORTED_MODULE_1___default().logo_text),
+                children: "ORACUL"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: (_logo_module_scss__WEBPACK_IMPORTED_MODULE_1___default().logo)
+            })
+        ]
+    }));
+};
+
+
+/***/ })
+
+};
+;
